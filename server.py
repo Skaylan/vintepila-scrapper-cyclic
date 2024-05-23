00b2deb36f2fe2fc83dd9f49a4766dd8d72b60a2
Lucas Oliveira <lucas_7tv@hotmail.com>
2024-05-22 22:53:21 -0300

    fix: bug with wsgi server, using gunicorn instead

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
+import sys
+from gunicorn.app.wsgiapp import run
 from app import app
 
 if __name__ == '__main__':
-    app.run()
+    sys.argv = "gunicorn --bind 0.0.0.0:5151 app:app".split()
+    sys.exit(run())
